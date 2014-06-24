c756dbc11bb39d492d4ece7adba627f311662702
astux7 <Asta.bevainyte@gmail.com>
2014-06-24 22:54:31 +0100

    tested empty input

--- lib/job_handler.rb
+++ lib/job_handler.rb
@@ -1,3 +1,13 @@
 class JobHandler
 
+  attr_accessor :input
+
+  def initialize(input = nil)
+    @input = input
+  end
+
+  def make_sequence
+    return "" if @input.empty?
+  end 
+  
 end
--- spec/job_handler_spec.rb
+++ spec/job_handler_spec.rb
@@ -2,4 +2,14 @@ require_relative '../lib/job_handler'
 
 describe JobHandler do
 
+  let(:job_handler) { JobHandler.new() }
+
+  it 'should init the JobHandler' do
+    expect(job_handler.input.nil?).to be_true
+  end
+
+  it 'returns empty string for empty input' do
+    job_handler.input = ""
+    expect(job_handler.make_sequence).to eq ""
+  end
 end
