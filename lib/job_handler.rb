87ee330dab41543e68c791dbde5c7166b416ea0f
astux7 <Asta.bevainyte@gmail.com>
2014-06-25 00:46:58 +0100

    added check of the string input

--- lib/job_handler.rb
+++ lib/job_handler.rb
@@ -1,13 +1,26 @@
 class JobHandler
 
-  attr_accessor :input
+  attr_accessor :input, :output
 
-  def initialize(input = nil)
+  JOB_UNIT_INPUT_TEMPLATE = /(^[a-z]{1}\s{1}?)(=>)(|(\s{1}?[a-z]{1}?))$/
+
+  def initialize(input = nil, output = "")
     @input = input
+    @output = output
   end
-
+  #make the sequence for output
   def make_sequence
-    return "" if @input.empty?
-  end 
-  
+    return @output if @input.empty?
+    raise "Unknown job squence" unless correct_job_sequence_input?
+    @input
+  end
+  #every job unit 'a=>b' separated with new line
+  def correct_job_sequence_input?
+    jobs = @input.split("\n")
+    !jobs.any?{|job| correct_job_unit?(job) == false }
+  end
+  #the user input checker: a => , a=>n, a=> n, a => h..
+  def correct_job_unit?(job)
+    JOB_UNIT_INPUT_TEMPLATE.match(job).nil? ? false : true
+  end
 end
--- spec/job_handler_spec.rb
+++ spec/job_handler_spec.rb
@@ -4,12 +4,28 @@ describe JobHandler do
 
   let(:job_handler) { JobHandler.new() }
 
-  it 'should init the JobHandler' do
+  it 'should init the JobHandler nil input and empty string for output' do
     expect(job_handler.input.nil?).to be_true
+    expect(job_handler.output.empty?).to be_true
   end
 
   it 'returns empty string for empty input' do
     job_handler.input = ""
     expect(job_handler.make_sequence).to eq ""
   end
+
+  it 'raise the error about unrecognized input' do
+    job_handler.input = "8=> p"
+    expect(lambda { job_handler.make_sequence} ).to raise_error(RuntimeError)
+    job_handler.input = "u => l\np =>8"
+    expect(lambda { job_handler.make_sequence} ).to raise_error(RuntimeError)
+  end
+
+  it 'should not raise the error if correct input' do
+    job_handler.input = "k => g"
+    expect(job_handler.make_sequence.empty?).to be_false
+    job_handler.input = "k => g\np=>"
+    expect(job_handler.make_sequence.empty?).to be_false
+  end
+
 end
